f660a94b6b27950ecb2a864ac88fb21c092e011d
roderik mogot <roderikmogot17@gmail.com>
2023-01-23 01:35:12 +0700

    added logic for adding new requests

--- features/dashboard/left-sidebar/index.tsx
+++ features/dashboard/left-sidebar/index.tsx
@@ -1,25 +1,76 @@
 import React, { useState } from "react";
+import { api } from "../../../src/utils/api";
 
 import useUserStore from "../../../store/use-user.store";
+
+const COLLECTION_DEFAULT = (email: string) => {
+  const UUID = Math.random().toString(36).substr(2, 9);
+  return {
+    name: "Test1",
+    method: "GET",
+    url: "",
+    queryParams: [
+      {
+        parameter: "",
+        value: "",
+      },
+    ],
+    authBasic: {
+      username: "",
+      password: "",
+    },
+    authBearer: {
+      token: "",
+    },
+    body: {
+      jsonContent: "",
+    },
+    uniqueId: UUID,
+    userEmail: email,
+  };
+};
 
 const Sidebar = () => {
   const email = useUserStore((state) => state.email);
 
+  const postCollections = api.collections.post.useMutation();
+  const getCollections = api.collections.get.useQuery({
+    email,
+  });
+
+  const [collections, setCollections] = useState([COLLECTION_DEFAULT(email)]);
   const [collectionsIdx, setCollectionsIdx] = useState(0);
-  const [collections, setCollections] = useState([
-    { name: "Test1", id: 0 },
-    { name: "Test2", id: 1 },
-    { name: "Test3", id: 2 },
-  ]);
 
   const handleCollectionTab = (id: number) => {
     setCollectionsIdx((_) => id);
   };
 
+  const handleAddCollection = () => {
+    const newCollection = COLLECTION_DEFAULT(email);
+    setCollections((prevCollections) => [...prevCollections, newCollection]);
+    postCollections.mutate({
+      name: newCollection.name,
+      method: newCollection.method,
+      url: newCollection.url,
+      queryParams: JSON.stringify(newCollection.queryParams),
+      authBasic: JSON.stringify(newCollection.authBasic),
+      authBearer: JSON.stringify(newCollection.authBearer),
+      body: JSON.stringify(newCollection.body),
+      uniqueId: newCollection.uniqueId,
+      userEmail: newCollection.userEmail,
+      createdAt: new Date().toISOString(),
+    });
+  };
+
+  console.log(collections);
+
   return (
     <div className="h-[90vh] w-[15%] overflow-auto">
       <div className="flex w-full flex-col">
-        <button className="bg-gray-600 px-4 py-2 font-bold text-white">
+        <button
+          className="bg-gray-600 px-4 py-2 font-bold text-white"
+          onClick={handleAddCollection}
+        >
           New
         </button>
         <div className="mt-4">
@@ -28,14 +79,12 @@ const Sidebar = () => {
             <ul className="flex w-full flex-col gap-2">
               {collections.map((collection, idx) => (
                 <li
-                  className={`${
-                    collectionsIdx === collection.id ? "bg-gray-600" : ""
-                  }`}
+                  className={`${collectionsIdx === idx ? "bg-gray-600" : ""}`}
                   key={idx}
                 >
                   <a
                     href="#"
-                    onClick={() => handleCollectionTab(collection.id)}
+                    onClick={() => handleCollectionTab(idx)}
                     className="flex w-full border-2 border-b-black border-l-transparent border-t-transparent border-r-transparent px-4 py-2 font-bold text-gray-300"
                   >
                     {collection.name}
