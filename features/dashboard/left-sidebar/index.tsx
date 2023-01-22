5d5bc8ca71fb5719810e9d3102952ce0ab8edca5
roderik mogot <roderikmogot17@gmail.com>
2023-01-22 09:46:41 +0700

    moved dashboard to features

---
+++ features/dashboard/left-sidebar/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+import useUserStore from "../../../store/user";
+
+const Sidebar = () => {
+  const email = useUserStore((state) => state.email);
+
+  const [collectionsIdx, setCollectionsIdx] = useState(0);
+  const [collections, setCollections] = useState([
+    { name: "Test1", id: 0 },
+    { name: "Test2", id: 1 },
+    { name: "Test3", id: 2 },
+  ]);
+
+  const handleCollectionTab = (id: number) => {
+    setCollectionsIdx((_) => id);
+  };
+
+  return (
+    <div className="h-[90vh] w-[15%] overflow-auto">
+      <div className="flex w-full flex-col">
+        <button className="bg-gray-600 px-4 py-2 font-bold text-white">
+          New
+        </button>
+        <div className="mt-4">
+          <div className="text-2xl font-bold">Collections</div>
+          <div className="mt-2">
+            <ul className="flex w-full flex-col gap-2">
+              {collections.map((collection, idx) => (
+                <li
+                  className={`${
+                    collectionsIdx === collection.id ? "bg-gray-600" : ""
+                  }`}
+                  key={idx}
+                >
+                  <a
+                    href="#"
+                    onClick={() => handleCollectionTab(collection.id)}
+                    className="flex w-full border-2 border-b-black border-l-transparent border-t-transparent border-r-transparent px-4 py-2 font-bold text-gray-300"
+                  >
+                    {collection.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ features/dashboard/right-sidebar/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+import rndJson from "../../../src/utils/random.json";
+
+const RightSidebar = () => {
+  const [resultIdx, setResultIdx] = useState(0);
+  return (
+    <div className="w-[40%]">
+      <div className="flex flex-row space-x-4">
+        <div className="text-xl font-bold">
+          <div>Status: </div>
+          <div></div>
+        </div>
+        <div className="text-xl font-bold">
+          <div>Size: </div>
+          <div></div>
+        </div>
+        <div className="text-xl font-bold">
+          <div>Time: </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="mt-4 w-full">
+        <ul className="flex gap-2">
+          <li>
+            <a
+              href="#"
+              onClick={() => setResultIdx(0)}
+              className={`px-4 py-2 font-bold ${
+                resultIdx === 0
+                  ? "border-2 border-b-black border-t-transparent border-l-transparent border-r-transparent"
+                  : "text-gray-300"
+              }`}
+            >
+              Response
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="mt-4 w-full">
+        <div className={resultIdx === 0 ? "block" : "hidden"}>
+          <pre>{JSON.stringify(rndJson, null, 2)}</pre>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightSidebar;
