dc101967dd1ea4d0532f5aac6c8d79a740aedb80
roderik mogot <roderikmogot17@gmail.com>
2023-01-23 01:42:59 +0700

    added collections router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "./trpc";
 import { userRouter } from "./routers/user";
+import { collectionsRouter } from "./routers/collections";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { userRouter } from "./routers/user";
  */
 export const appRouter = createTRPCRouter({
   user: userRouter,
+  collections: collectionsRouter,
 });
 
 // export type definition of API
